f911e61f34a526afe8b18f7b1664e001dd6ca7ea
Deathrovv <aro.maru7ian@gmail.com>
2021-10-23 20:06:57 +0200

    emotional detector done. Need to split functionality into chat and view pages

--- only-farts/src/App.js
+++ only-farts/src/App.js
@@ -11,6 +11,7 @@ import { useState, useRef, useEffect } from "react"; //use effect will be used f
 
 //Attribute emotion function sends the data to firebase
 
+//for view side
 const DisplayMessages = ({ message }) => {
   return (
     <>
@@ -20,12 +21,53 @@ const DisplayMessages = ({ message }) => {
   );
 };
 
-const App = () => {
-  const [messages, setMessages] = useState([]);
+//for chat side
+const HandleNewMessage = async (e) => {
   const [formValue, setFormValue] = useState("");
   const [emotion, setEmotion] = useState("");
-  // console.log(messages);
+  e.preventDefault();
+
+  const emotionDetector = (message) => {
+    const sad = ["sad", "triste", "pathetic", "ugly"];
+    const happy = ["happy", "joy", "alegria", "feliz"];
+    const messageChecker = (array) =>
+      array.some((word) => message.includes(word)) && setEmotion(`${array[0]}`);
+    messageChecker(sad);
+    messageChecker(happy);
+  };
+  const collectionRef = collection(db, "messages");
+  // emotionDetector(formValue);
+  // setEmotion(formValue);
+  const payload = { message: formValue, emotion: emotion };
+  const docRef = await addDoc(collectionRef, payload);
   // console.log(formValue);
+  setFormValue("");
+  console.log(`The emotion is ${emotion}`);
+  setEmotion("");
+  console.log(`The ID of this message is ${docRef.id}`);
+
+  return (
+    <form onSubmit={HandleNewMessage}>
+      <input
+        value={formValue}
+        onChange={(e) => setFormValue(e.target.value)}
+        placeholder="say something nice"
+      />
+
+      <button
+        type="submit"
+        onClick={() => emotionDetector(formValue)}
+        disabled={!formValue}
+      >
+        🕊️
+      </button>
+    </form>
+  );
+};
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
   useEffect(
     () =>
       onSnapshot(collection(db, "messages"), (snapshot) =>
@@ -34,28 +76,7 @@ const App = () => {
     []
   );
 
-  const emotionDetector = (message) => {
-    const sad = ["triste", "sad", "pathetic", "ugly"];
-    const happy = ["joy", "alegria", "feliz"];
-    message.includes("test") && setEmotion("success!");
-    message.split(" ").some((x) => x === "triste") && setEmotion("sad");
-    // message.split(" ").some(...happy) && setEmotion("happy");
-  };
-
-  const handleNewMessage = async (e) => {
-    e.preventDefault();
-    const collectionRef = collection(db, "messages");
-    // emotionDetector(formValue);
-    // setEmotion(formValue);
-    const payload = { message: formValue, emotion: emotion };
-    const docRef = await addDoc(collectionRef, payload);
-    // console.log(formValue);
-    setFormValue("");
-    console.log(`The emotion is ${emotion}`);
-    setEmotion("");
-    console.log(`The ID of this message is ${docRef.id}`);
-  };
-  // const handleNewMessage = async (e) => {
+  // const HandleNewMessage = async (e) => {
   //   e.preventDefault();
   //   const docRef = doc(db, "messages", "message003");
   //   const payload = { message: formValue, emotion: "promising" };
@@ -66,24 +87,9 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>OnlyFarts</h1>
-        <form onSubmit={handleNewMessage}>
-          <input
-            value={formValue}
-            onChange={(e) => setFormValue(e.target.value)}
-            placeholder="say something nice"
-          />
-
-          <button
-            type="submit"
-            onClick={() => emotionDetector(formValue)}
-            disabled={!formValue}
-          >
-            🕊️
-          </button>
-        </form>
-
-        {/* <button onClick={handleNewMessage}>New Message</button> */}
-        <h2>{formValue}</h2>
+        <HandleNewMessage />
+        {/* <button onClick={HandleNewMessage}>New Message</button> */}
+        {/* <h2>{formValue}</h2> */}
         <ul>
           {messages.map((message) => (
             <li key={message.id}>
