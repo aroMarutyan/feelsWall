4cd0a885610bab0dc3f445bf26a4a625adab260d
Deathrovv <aro.maru7ian@gmail.com>
2021-10-24 13:41:26 +0200

    public website initial commit

--- only-farts/src/App.js
+++ only-farts/src/App.js
@@ -11,7 +11,6 @@ import { useState, useRef, useEffect } from "react"; //use effect will be used f
 
 //Attribute emotion function sends the data to firebase
 
-//for view side
 const DisplayMessages = ({ message }) => {
   return (
     <>
@@ -21,52 +20,10 @@ const DisplayMessages = ({ message }) => {
   );
 };
 
-//for chat side
-const HandleNewMessage = async (e) => {
-  const [formValue, setFormValue] = useState("");
-  const [emotion, setEmotion] = useState("");
-  e.preventDefault();
-
-  const emotionDetector = (message) => {
-    const sad = ["sad", "triste", "pathetic", "ugly"];
-    const happy = ["happy", "joy", "alegria", "feliz"];
-    const messageChecker = (array) =>
-      array.some((word) => message.includes(word)) && setEmotion(`${array[0]}`);
-    messageChecker(sad);
-    messageChecker(happy);
-  };
-  const collectionRef = collection(db, "messages");
-  // emotionDetector(formValue);
-  // setEmotion(formValue);
-  const payload = { message: formValue, emotion: emotion };
-  const docRef = await addDoc(collectionRef, payload);
-  // console.log(formValue);
-  setFormValue("");
-  console.log(`The emotion is ${emotion}`);
-  setEmotion("");
-  console.log(`The ID of this message is ${docRef.id}`);
-
-  return (
-    <form onSubmit={HandleNewMessage}>
-      <input
-        value={formValue}
-        onChange={(e) => setFormValue(e.target.value)}
-        placeholder="say something nice"
-      />
-
-      <button
-        type="submit"
-        onClick={() => emotionDetector(formValue)}
-        disabled={!formValue}
-      >
-        🕊️
-      </button>
-    </form>
-  );
-};
-
 const App = () => {
   const [messages, setMessages] = useState([]);
+  const [formValue, setFormValue] = useState("");
+  const [emotion, setEmotion] = useState("");
 
   useEffect(
     () =>
@@ -76,7 +33,38 @@ const App = () => {
     []
   );
 
-  // const HandleNewMessage = async (e) => {
+  const emotionDetector = (message) => {
+    const sad = ["sad", "triste", "pathetic", "ugly"];
+    const happy = ["happy", "joy", "alegria", "feliz"];
+    const messageChecker = (array) => {
+      return (
+        array.some((word) => message.includes(word)) &&
+        setEmotion(`${array[0]}`)
+      );
+    };
+
+    // message.includes("test") && setEmotion("success!");
+    // message.split(" ").map((word) => word) && setEmotion("sad");
+    // sad.some((word) => message.includes(word)) && setEmotion("sad");
+    // happy.some((word) => message.includes(word)) && setEmotion("happy"); //refactor this later
+    messageChecker(sad);
+    messageChecker(happy);
+  };
+
+  const handleNewMessage = async (e) => {
+    e.preventDefault();
+    const collectionRef = collection(db, "messages");
+    // emotionDetector(formValue);
+    // setEmotion(formValue);
+    const payload = { message: formValue, emotion: emotion };
+    const docRef = await addDoc(collectionRef, payload);
+    // console.log(formValue);
+    setFormValue("");
+    console.log(`The emotion is ${emotion}`);
+    setEmotion("");
+    console.log(`The ID of this message is ${docRef.id}`);
+  };
+  // const handleNewMessage = async (e) => {
   //   e.preventDefault();
   //   const docRef = doc(db, "messages", "message003");
   //   const payload = { message: formValue, emotion: "promising" };
@@ -87,9 +75,24 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>OnlyFarts</h1>
-        <HandleNewMessage />
-        {/* <button onClick={HandleNewMessage}>New Message</button> */}
-        {/* <h2>{formValue}</h2> */}
+        <form onSubmit={handleNewMessage}>
+          <input
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+            placeholder="say something nice"
+          />
+
+          <button
+            type="submit"
+            onClick={() => emotionDetector(formValue)}
+            disabled={!formValue}
+          >
+            🕊️
+          </button>
+        </form>
+
+        {/* <button onClick={handleNewMessage}>New Message</button> */}
+        <h2>{formValue}</h2>
         <ul>
           {messages.map((message) => (
             <li key={message.id}>
